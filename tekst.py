bdf6c18729528d3b92723670cbd22b2219a8454f
blazejub <90357483+blazejub@users.noreply.github.com>
2021-09-22 19:32:52 +0200

    Delete tekst.py

--- tekst.py
+++
@@ -1,8 +0,0 @@
-print ('programuje w \n Pythonie')
-print ('i nie tylko')
-print('dodawanie',23+5)
-# to jest komentarz...
-# funkcja Print pozwala łączyć wyświetlanie napisów, wyrażeń i zmiennych
-# umieszczanie w tekście sekwencji znaków \n wymusza przejscie do następej lini
-# umieszczenie znaku \n na poczatku zdania wymusza dodatkowy odstęp miedzy linijkami
-input ('\n\n Aby zakonczyc, nacisnij ENTER')
